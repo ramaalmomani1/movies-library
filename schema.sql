0c44136b83f02feb96cdda3aff6d03aceef73c2c
ramaalmoamni1 <raalmomani17197@eng.just.edu.jo>
2023-05-18 20:04:11 +0000

    new update

--- schema.sql
+++ schema.sql
@@ -5,4 +5,17 @@ create table if not exists info (
   
 );
 
+
+
+create table if not exists front_movie_info (
+  id serial primary key,
+  movie_id integer,
+  title varchar(255),
+  poster_path varchar(1000),
+  comments varchar(1000)
+);
+
+
+
+
 insert into info(title, comments) values('Titanic', '1997 film')
